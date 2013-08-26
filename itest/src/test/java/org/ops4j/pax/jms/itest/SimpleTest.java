4e6c8a90bcda4e7345bc1f1726919e72a03d6974
laeubi <laeubi@laeubi-soft.de>
2013-08-26 18:00:45 +0200

    PAXJMS-8 fix javadoc and licence

--- itest/src/test/java/org/ops4j/pax/jms/itest/SimpleTest.java
+++ itest/src/test/java/org/ops4j/pax/jms/itest/SimpleTest.java
@@ -1,5 +1,19 @@
 /**
- * 
+ * Copyright 2013 OPS4J
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.ops4j.pax.jms.itest;
 
@@ -10,7 +24,7 @@ import javax.jms.Session;
 import org.junit.Test;
 
 /**
- * @author Christoph Läubrich
+ * The simple test just makes sure objects getting created/injected
  */
 public class SimpleTest extends ITestBase {
 
